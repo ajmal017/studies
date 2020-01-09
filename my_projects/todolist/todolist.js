0f78d88499af02d5360751f3aebcf2db86715b51
jaehyeon48 <imjaehyeon48@gmail.com>
2020-01-09 15:49:00 +0900

    some minor changes

--- my_projects/todolist/todolist.js
+++ my_projects/todolist/todolist.js
@@ -69,7 +69,6 @@ function addItem() {
 
     xhr.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
             sortList();
             loadList();
             todoInput.value = '';
